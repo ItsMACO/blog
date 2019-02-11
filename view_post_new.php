9de103337be86dd7cbf8ff5847625d2bd70ee8cd
Matej Palo <macoplays8@gmail.com>
2019-02-11 08:30:36 +0100

    Update view_post_new.php

--- view_post_new.php
+++ view_post_new.php
@@ -9,13 +9,43 @@
   $pid = $_GET['pid'];
   $sql = "SELECT * FROM posts WHERE id=$pid LIMIT 1";
   $result = mysqli_query($con, $sql) or die(mysqli_error($con));
-  $row = mysqli_fetch_assoc($result) {
-  $pagetitle = $row['title'];
-  $image = $row['image'];
-      }
-  }
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $id = $row['id'];
+        $title = $row['title'];
+        $date = $row['date'];
+        $content = $row['content'];
+        $author = $row['author'];
+        $image = $row['image'];
+        $flair = $row['flair'];
+      
+      if(is_int($author)) {
+          $sql_profile = "SELECT * FROM users WHERE username='$author'";
+          $result_profile = mysqli_query($con, $sql_profile) or die(mysqli_error($con));
+          if (mysqli_num_rows($result_profile) > 0) {
+              while ($row = mysqli_fetch_assoc($result_profile)) {
+                  $userid = $row['id'];
+                  $username = $row['username'];
+                  $email = $row['email'];
+                } 
+            }
+          $output = $bbcode->Parse($content);
+          if(isset($_SESSION['username']) && $_SESSION['username'] == $author) {
+          $edit_delete = "<div>
+              <a href='#stats' class='button button1 modal-trigger'>STATS</a>
+              <a href='edit_post.php?pid=$pid' class='button button2'>EDIT</a>&nbsp;
+              <a href='del_post.php?pid=$pid' class='button button3'>DELETE</a>
+              </div>";
+          } else {
+              $edit_delete = "<div>";
+          }
+        }
+    }
+}
+require_once 'nbbc.php';
+$bbcode = new BBCode;
 ?>
-?>
+
 <title><?php echo $pagetitle; ?></title>
 </head>
 <body>
@@ -33,6 +63,8 @@
           </div>
       </div>
       
+      
+        
     </div>
   </div>
 </div>
